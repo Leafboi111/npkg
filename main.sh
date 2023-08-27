d5efee4572b1d7812a258be5a647a6f97cbebd8e
Leafboi <110484176+Leafboi111@users.noreply.github.com>
2023-08-27 10:35:49 -0700

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+Copyright (C) <year>  <name of author>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+function install {
+    cd pkgs
+    read -p "Add git repository URL: " pkglink
+    git clone $pkglink
+    cd ..
+}
+
+function execute {
+    read -p "Package to execute: " pkgrun
+    bash pkgs/$pkgrun/main.sh
+}
+
+function remove {
+    read -p "Package to remove: " pkgrun
+    rm -rf pkgs/$pkgrun
+}
+
+function main {
+    read -p "Install, execute, or remove package [i/e/r]: " sq
+
+    case $sq in
+        [iI] ) install;;
+            #break;;
+        [eE] ) execute;;
+            #break;;
+        [rR] ) remove;;
+            #break;;
+        * ) break
+    esac
+}
+
+chmod 777 -R *
+while true
+do
+  main
+done
